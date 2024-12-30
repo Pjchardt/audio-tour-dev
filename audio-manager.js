cda90bc2708c8894652eb640d1cd2e040e461100
VRT-Richard <richie@ralphvr.com>
2024-12-29 21:43:01 -0500

    stuff

--- audio-manager.js
+++ audio-manager.js
@@ -6,14 +6,14 @@ function createSpatialSound(audioPath, onReady) {
     return new Howl({
       src: [audioPath],
       loop: true,
-      volume: 1.0,
+      volume: 3.0,
       html5: false, 
       pannerAttr: {
         panningModel: 'HRTF',
         distanceModel: 'exponential',
         refDistance: 1,
         maxDistance: 100,
-        rolloffFactor: 1,
+        rolloffFactor: 3,
       },
       onload: onReady,
       onplay: () => console.log('Looped sound is playing.')
